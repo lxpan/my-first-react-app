5164c989d6d7e922f962e20d05a3e7dce6f226e2
Luoxi Pan <l.panpax@gmail.com>
2023-02-07 21:38:21 +1100

    Convert Clock from function to class

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
