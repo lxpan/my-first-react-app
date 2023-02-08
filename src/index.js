b8787f3402d6f32cfdaf8267063baff0b9717e49
Luoxi Pan <l.panpax@gmail.com>
2023-02-08 15:47:10 +1100

    Add Reservation to show multiple inputs

--- src/index.js
+++ src/index.js
@@ -86,6 +86,8 @@ class FlavorForm extends React.Component {
     event.preventDefault();
   }
 
+  
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
@@ -104,12 +106,57 @@ class FlavorForm extends React.Component {
   }
 }
 
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <label>
+          Is going:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            // value={this.state.numberOfGuests}
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+      </form>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <NameForm />
-    <EssayForm />
-    <FlavorForm />
+    <Reservation />
   </>
 );
 
