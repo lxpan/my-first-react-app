50323dc8d4ea40fed7c910d11b50018e19b6d3b7
Luoxi Pan <l.panpax@gmail.com>
2023-02-08 13:48:16 +1100

    Replace people array with more detailed data

--- src/index.js
+++ src/index.js
@@ -4,16 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const people = [
-  'Creola Katherine Johnson: mathematician',
-  'Mario José Molina-Pasquel Henríquez: chemist',
-  'Mohammad Abdus Salam: physicist',
-  'Percy Lavon Julian: chemist',
-  'Subrahmanyan Chandrasekhar: astrophysicist'
-];
+const people = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+}, {
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',  
+}, {
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+}];
 
 function List() {
-  const listItems = people.map(person => <li>{person}</li>);
+  const listItems = people.map(person => <li>{person.name}</li>);
   return <ul>{listItems}</ul>;
 }
 
